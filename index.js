2b6e8de988e9c2f3c6b92d37fec24fd2d6092c0d
King <133107592+BlackFoxWeb@users.noreply.github.com>
2024-10-16 22:59:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Happy developing ✨')
+document.addEventListener('mousemove', (e) => {
+    // Создаем новый элемент для следа
+    const trail = document.createElement('div');
+    trail.classList.add('neon-trail');
+
+    // Задаем позицию trail в зависимости от позиции курсора
+    trail.style.left = `${e.pageX}px`;
+    trail.style.top = `${e.pageY}px`;
+
+    // Добавляем элемент на страницу
+    document.body.appendChild(trail);
+
+    // Удаляем элемент через 0.5 секунд, когда анимация завершится
+    setTimeout(() => {
+        trail.remove();
+    }, 500);
+});
